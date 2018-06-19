380af5dbd3f9649be841a7d4b769497296b9750f
Berkay <berkayyalcin7@icloud.com>
2018-06-19 04:06:50 +0300

    Updated

---
+++ DP/Observer/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kendisine abone olan sistemlerin bir işlem olduğunda Devreye girmesi
+            // Örn : Alışveriş sisteminde ürün fiyatı düştüğünde müşterinin haberdar edilmesi..
+            ProductManager pdManager=new ProductManager();
+
+            //Çalışanlar Attach ile seçtiğimiz observer ile bilgilendirildi..
+            pdManager.Attach(new EmployeeObserver());
+            pdManager.UpdatePrice();
+
+            Console.ReadLine();
+        }
+    }
+
+    class ProductManager
+    {
+        List<Observer> _observers=new List<Observer>();
+        public void UpdatePrice()
+        {
+            Console.WriteLine("Product price Changed !");
+            Notify();
+        }
+
+        public void Attach(Observer observer)
+        {
+            _observers.Add(observer);
+        }
+        public void Detach(Observer observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        private void Notify()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update();
+            }
+        }
+
+
+    }
+
+    //Observer nesnesi
+    abstract class Observer
+    {
+        public abstract void Update();
+    }
+
+    class CustomerObserver : Observer
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Message to customer : Product price changed !");
+        }
+    }
+
+    class EmployeeObserver : Observer
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Message to employee : Product price changed !");
+        }
+    }
+
+}
---
+++ DP/Strategy/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Strateji belirleyip o metodun buna göre çalıştırılması
+            //İş katmanında belli bi mevzuata göre hesaplama  : Bankacılık - Kredi 
+            CustomerManager cm=new CustomerManager();
+            cm.CreditCalculatorBase=new After2010CreditCalculator();
+            //Obje referans hatası vereceğiz metodu çağırmadan önce üste
+            cm.SaveCredit();
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class CreditCalculatorBase
+    {
+        //normal şartlarda if-else koşullarıyla bir sürü işlem yapılacaktı 
+        public abstract void Calculate();
+    }
+
+    class Before2010CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit Calculated using before 2010");
+        }
+    }
+
+    class After2010CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit Calculated using after 2010");
+        }
+    }
+
+    //kullanılacak ortam - İş katmanında
+    class CustomerManager
+    {
+        //Base üzerinden işlemi gerçekleştiriiyor olacağzı
+        public CreditCalculatorBase CreditCalculatorBase { get; set; }
+        public void SaveCredit()
+        {
+            Console.WriteLine("CManager Business");
+            CreditCalculatorBase.Calculate();
+        }
+    }
+
+}
